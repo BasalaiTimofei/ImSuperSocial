4b3be0f3cf5ff6f0352c19473bce2ef057bb2d19
Timofei Basalai <>
2018-11-05 19:49:54 +0300

    Add model: CommentDb

---
+++ Legendary.Data/Models/Video/CommentDb.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Legendary.Data.Models.Video
+{
+    public class CommentDb
+    {
+        public string CommentId { get; set; }
+        public string UserId { get; set; }
+        public string VideoId { get; set; }
+        public string Comment { get; set; }
+        public DateTime DateCreate { get; set; }
+    }
+}
