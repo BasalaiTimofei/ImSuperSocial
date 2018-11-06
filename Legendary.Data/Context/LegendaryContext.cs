2a2f293c33c9eea0f3bc1bf664be02e4cfdc1a07
Timofei Basalai <>
2018-11-06 03:16:52 +0300

    Add Interfaces

--- Legendary.Data/Context/LegendaryContext.cs
+++ Legendary.Data/Context/LegendaryContext.cs
@@ -3,20 +3,26 @@ using Legendary.Data.Models.Video;
 
 namespace Legendary.Data.Context
 {
-    using System;
     using System.Data.Entity;
-    using System.Linq;
 
     public class LegendaryContext : DbContext
     {
         public LegendaryContext()
-            : base("name=LegendaryContext")
+            : base("LegendaryContext")
         {
+            Database.SetInitializer<LegendaryContext>(new CreateDatabaseIfNotExists<LegendaryContext>());
         }
 
         public DbSet<CategoryDb> Categories { get; set; }
         public DbSet<CommentDb> Comments { get; set; }
         public DbSet<VideoDb> Video { get; set; }
         public DbSet<UserDb> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new VideoDbConfiguration());
+            modelBuilder.Configurations.Add(new CategoryDbConfiguration());
+            modelBuilder.Configurations.Add(new CommentDbConfiguration());
+        }
     }
 }
